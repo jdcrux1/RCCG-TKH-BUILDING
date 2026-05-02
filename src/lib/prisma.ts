17813af2d6a7c0ef5d78911180140d241f43a8e1
macbook air <macbookair@Jays-Macbook.local>
2026-05-03 00:02:08 +0100

    Add environment variable fallback for build phase and provided env vars text file

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,7 +6,7 @@ export const prisma = globalForPrisma.prisma || new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   datasources: {
     db: {
-      url: process.env.DATABASE_URL,
+      url: process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/dummy",
     },
   },
 });
